11f21d7b44a5e8ce88fc73e6246be1f5e586d84d
ARGEN03 <argen.arzykulov03@gmail.com>
2023-11-21 21:27:09 +0600

    fix commit

--- check9.py
+++ check9.py
@@ -31,7 +31,7 @@ class ToDo(CreateMixin, ReadMixin, UpdateMixin, DeleteMixin):
         from datetime import datetime
         deadline = datetime.strptime(deadline, '%d/%m/%Y')
         today = datetime.now()
-        day = (deadline - today).days
+        day = (today - deadline).days
         return day
 
 task = ToDo()
@@ -45,75 +45,75 @@ print(task.set_deadline('31/12/2021'))
 print(task.todos)
 
 "=2="
-class Person:
-    __name = None
-    __last_name = None
-    __age = None
-    __email = None
+# class Person:
+#     __name = None
+#     __last_name = None
+#     __age = None
+#     __email = None
 
-    @property
-    def name(self):
-        return self.__name
+#     @property
+#     def name(self):
+#         return self.__name
     
-    @name.setter
-    def name(self,new_name):
-        self.__name = new_name
-        return self.__name
+#     @name.setter
+#     def name(self,new_name):
+#         self.__name = new_name
+#         return self.__name
     
-    @property
-    def last_name(self):
-        return self.__last_name
+#     @property
+#     def last_name(self):
+#         return self.__last_name
     
-    @last_name.setter
-    def last_name(self,new_last_name):
-        self.__last_name = new_last_name
-        return self.__last_name
+#     @last_name.setter
+#     def last_name(self,new_last_name):
+#         self.__last_name = new_last_name
+#         return self.__last_name
     
-    @property
-    def age(self):
-        return self.__age
+#     @property
+#     def age(self):
+#         return self.__age
     
-    @age.setter
-    def age(self,new_age):
-        self.__age = new_age
-        return self.__age
+#     @age.setter
+#     def age(self,new_age):
+#         self.__age = new_age
+#         return self.__age
     
-    @property
-    def email(self):
-        return self.__email
+#     @property
+#     def email(self):
+#         return self.__email
     
-    @email.setter
-    def email(self,new_email):
-        self.__email = new_email
-        return self.__email
+#     @email.setter
+#     def email(self,new_email):
+#         self.__email = new_email
+#         return self.__email
 
-john = Person()
-print(john.name) # None
-print(john.last_name) # None
-print(john.age) # None
-print(john.email) # None
-john.name = 'John'
-john.last_name = 'Snow'
-john.age = 30
-john.email = 'johnsnow@gmail.com'
-print(john.name) # John
-print(john.last_name) # Snow
-print(john.age) # 30
-print(john.email) # johnsnow@gmail.com
+# john = Person()
+# print(john.name) # None
+# print(john.last_name) # None
+# print(john.age) # None
+# print(john.email) # None
+# john.name = 'John'
+# john.last_name = 'Snow'
+# john.age = 30
+# john.email = 'johnsnow@gmail.com'
+# print(john.name) # John
+# print(john.last_name) # Snow
+# print(john.age) # 30
+# print(john.email) # johnsnow@gmail.com
 
 "=3="
-class Dog:
-    def voice(self):
-        print('Гав')
+# class Dog:
+#     def voice(self):
+#         print('Гав')
 
-class Cat:
-    def voice(self):
-        print('Мяу')
-rex = Dog()
-barsik = Cat()
+# class Cat:
+#     def voice(self):
+#         print('Мяу')
+# rex = Dog()
+# barsik = Cat()
 
-def to_pet(animal):
-    animal.voice()
+# def to_pet(animal):
+#     animal.voice()
 
-to_pet(rex)
-to_pet(barsik)
+# to_pet(rex)
+# to_pet(barsik)
